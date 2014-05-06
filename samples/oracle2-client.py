38466ddea6a97f7e4be73695b8614e5e84e5e590
Francois Chagnon <francois.chagnon@jadedpixel.com>
2014-05-05 21:00:18 -0400

    add client that can recover a secret encrypted with a block cipher (sample 2).

---
+++ samples/oracle2-client.py
@@ -0,0 +1,42 @@
+import socket
+import struct
+import string
+from common import send_blob, recv_blob
+
+from compression_oracle import CompressionOracle, TwoTriesBlockCipherGuess
+
+HOST = '127.0.0.1'    # The remote host
+PORT = 30001          # The same port as used by the server
+
+class Client(CompressionOracle):
+
+	def __init__(self, seed):
+		CompressionOracle.__init__(self, seed=seed, 
+			alphabet=string.printable, max_threads=3, 
+			lookaheads=0, retries=2, guess_provider=TwoTriesBlockCipherGuess)
+		self.s = None
+		return
+
+	def prepare(self):
+		return
+
+	def prepare_complement(self):
+		return '\x00\xFF'*50
+
+	def oracle(self, data):
+		""" send 'data' to the oracle and retreived the compressed length """
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((HOST, PORT))
+
+		send_blob(s, data)
+		data = recv_blob(s)
+
+		s.close()
+		return len(data)
+
+	def cleanup(self):
+		return
+
+c = Client(seed='secret=')
+c.run()
